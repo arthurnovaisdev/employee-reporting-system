a69edfed91afbe90fc0f2589864b3a9e69f5a82c
arthurnovaisdev <arthurnovaispeixoto@gmail.com>
2026-08-26 11:55:18 -0300

    first commit

---
+++ src/main/java/com/mbfreire/employee_reporting/EmployeeReportingApplication.java
@@ -0,0 +1,13 @@
+package com.mbfreire.employee_reporting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeReportingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeReportingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mbfreire/employee_reporting/EmployeeReportingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mbfreire.employee_reporting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeReportingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
