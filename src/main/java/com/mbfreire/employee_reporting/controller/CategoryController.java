c338a33561298daefd3fd53d4e40064f1d9f4d0f
arthurnovaisdev <arthurnovaispeixoto@gmail.com>
2026-08-27 16:00:29 -0300

    CategoryController

---
+++ src/main/java/com/mbfreire/employee_reporting/controller/CategoryController.java
@@ -0,0 +1,31 @@
+package com.mbfreire.employee_reporting.controller;
+
+import com.mbfreire.employee_reporting.dto.request.CategoryRequestDTO;
+import com.mbfreire.employee_reporting.dto.response.CategoryResponseDTO;
+import com.mbfreire.employee_reporting.service.CategoryService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/categories")
+@RequiredArgsConstructor
+public class CategoryController {
+
+    private final CategoryService categoryService;
+
+    @PostMapping
+    public ResponseEntity<CategoryResponseDTO> create(@Valid @RequestBody CategoryRequestDTO dto) {
+        CategoryResponseDTO response = categoryService.create(dto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<CategoryResponseDTO>> findAll() {
+        return ResponseEntity.ok(categoryService.findAll());
+    }
+}
